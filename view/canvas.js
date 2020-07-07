b802d48c7f15cd5ccf7ed8bd30f06364ed44d337
SekiT <takaaki.seki@access-company.com>
2020-07-07 15:18:31 +0900

    Assign fixed values once

--- view/canvas.js
+++ view/canvas.js
@@ -7,15 +7,17 @@ const { createElement } = dependencies.globals;
 const element = createElement('canvas');
 element.width = 400;
 element.height = 400;
+Object.assign(element.style, {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+});
 
 export const context = element.getContext('2d');
 
 export const canvasView = view({ width: 0 }, (render) => ({ width }) => {
   Object.assign(element.style, {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
     width: `${width}px`,
     height: `${width}px`,
   });
