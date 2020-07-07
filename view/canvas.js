34903e17a5f9d71cba72932f7f6d7ba63320153d
SekiT <takaaki.seki@access-company.com>
2020-07-07 18:38:29 +0900

    Draw player buide and fix order

--- view/canvas.js
+++ view/canvas.js
@@ -2,7 +2,9 @@ import { view } from '@/lib/view';
 import windowSize from '@/subject/windowSize';
 import dependencies from 'dependencies';
 
-const { pi, createElement } = dependencies.globals;
+const {
+  pi, cos, sin, createElement,
+} = dependencies.globals;
 
 const element = createElement('canvas');
 element.width = 410;
@@ -38,14 +40,7 @@ context.arc(205, 205, 200, 0, 2 * pi);
 context.fill();
 context.closePath();
 
-// Center dot
-context.beginPath();
-context.fillStyle = 'white';
-context.arc(205, 205, 3, 0, 2 * pi);
-context.fill();
-context.closePath();
-
-// Guide line
+// Start/Goal line
 context.beginPath();
 context.strokeStyle = 'white';
 context.lineWidth = 1;
@@ -54,13 +49,35 @@ context.lineTo(405, 205);
 context.stroke();
 context.closePath();
 
+const playerAngle = -pi / 6;
+const playerRadius = 100;
+
+const px = 205 + playerRadius * cos(playerAngle);
+const py = 205 + playerRadius * sin(playerAngle);
+
+// Guide line
+context.beginPath();
+context.strokeStyle = 'red';
+context.lineWidth = 1;
+context.moveTo(205, 205);
+context.lineTo(205 + 200 * cos(playerAngle), 205 + 200 * sin(playerAngle));
+context.stroke();
+context.closePath();
+
 // Player
 context.beginPath();
-const gradient = context.createRadialGradient(305, 205, 0, 305, 205, 20);
+const gradient = context.createRadialGradient(px, py, 0, px, py, 20);
 gradient.addColorStop(0, 'red');
 gradient.addColorStop(1, 'rgba(255, 0, 0, 0)');
 context.fillStyle = gradient;
-context.arc(305, 205, 20, 0, 2 * pi);
+context.arc(px, py, 20, 0, 2 * pi);
+context.fill();
+context.closePath();
+
+// Center dot
+context.beginPath();
+context.fillStyle = 'white';
+context.arc(205, 205, 3, 0, 2 * pi);
 context.fill();
 context.closePath();
 
