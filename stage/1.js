a0049543be11fea1babd4d65943cfd5f38edf82f
SekiT <takaaki.seki@access-company.com>
2020-07-08 22:46:54 +0900

    Make dummy stage short and easier

--- stage/1.js
+++ stage/1.js
@@ -8,22 +8,20 @@ const pi2 = pi * 2;
 export default (state) => {
   const enemies = [
     state.enemies,
-    random() < 0.1
-      ? [swimOrb(random() * pi2, random() * boardRadius, random() * 0.02, 6 + random() * 4)]
-      : [],
-    random() < 0.1
-      ? [
-        linearOrb(
-          center + (2 * random() - 1) * boardRadius,
-          center + (2 * random() - 1) * boardRadius,
-          random() * pi2,
-          1 + random(),
-          6 + random() * 4,
-          'white',
-          'blue',
-        ),
-      ]
-      : [],
+    random() >= 0.06 ? [] : [
+      swimOrb(random() * pi2, random() * boardRadius, random() * 0.02, 6 + random() * 4),
+    ],
+    random() >= 0.05 ? [] : [
+      linearOrb(
+        center + (2 * random() - 1) * boardRadius,
+        center + (2 * random() - 1) * boardRadius,
+        random() * pi2,
+        1 + random(),
+        6 + random() * 4,
+        'white',
+        'blue',
+      ),
+    ],
   ].flat();
   return { ...state, enemies };
 };
