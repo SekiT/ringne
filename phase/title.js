564b8f8a6ef20257d198e9257fecf8e2ed1e320f
SekiT <takaaki.seki@access-company.com>
2020-07-08 16:28:26 +0900

    Death to back to main

--- phase/title.js
+++ phase/title.js
@@ -1,7 +1,15 @@
 import ids from './ids';
 
-export default () => () => ({
+export default (time = 0) => () => (time < 120 ? {
   nextId: ids.title,
-  nextArgs: [],
+  nextArgs: [time + 1],
   stateUpdate: {},
+} : {
+  nextId: ids.main,
+  nextArgs: [0],
+  stateUpdate: {
+    playerAngle: 0,
+    playerRadius: 100,
+    enemies: [],
+  },
 });
