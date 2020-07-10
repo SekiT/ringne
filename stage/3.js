08082323eadef46a1a630e3bbbb66caf1545de16
SekiT <takaaki.seki@access-company.com>
2020-07-10 19:09:55 +0900

    Fix stage3

--- stage/3.js
+++ stage/3.js
@@ -1,3 +1,5 @@
-const three = (time = 0) => () => three(time + 1);
+import none from '@/event/none';
+
+const three = (time = 0) => () => ({ enemies: [], nextStage: three(time + 1), evt: none() });
 
 export default three;
