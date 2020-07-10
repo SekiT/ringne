42146db691c4015f794c96b7332f616d53e5682b
SekiT <takaaki.seki@access-company.com>
2020-07-10 18:55:08 +0900

    Reset transform in none.afterEffect

--- event/none.js
+++ event/none.js
@@ -1,10 +1,15 @@
 import makeEvent from './makeEvent';
 import ids from './ids';
 
+const afterEffect = (_props, _eventTime, context) => {
+  context.resetTransform();
+};
+
 export default () => makeEvent({
   id: ids.none,
   name: '-',
   wait: Number.POSITIVE_INFINITY,
   duration: 0,
   props: {},
+  afterEffect,
 });
