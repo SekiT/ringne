91f947cde5ba8ec06ac58afd209a631460f474ba
SekiT <takaaki.seki@access-company.com>
2020-07-20 23:56:20 +0900

    Reform main function into state reduction style

--- phase/main.js
+++ phase/main.js
@@ -17,8 +17,108 @@ const {
   pi2, cos, sin, min, max, random,
 } = dependencies.globals;
 
-const moveEnemies = (enemies, px, py) => (
-  enemies.reduce((acc, enemy) => {
+const getInputsIntoState = () => ({ state: getInputs() });
+
+const handleEscape = ({ escape, mode }) => ({
+  returns: escape ? {
+    nextId: ids.title,
+    nextArgs: [],
+    stateUpdate: {
+      ...initialState(),
+      mode,
+    },
+  } : false,
+});
+
+const handlePause = ({ pause, pauseTime }) => ({
+  returns: pause && pauseTime > 10 ? {
+    nextId: ids.pause,
+    nextArgs: [0],
+    stateUpdate: {},
+  } : false,
+});
+
+const runInputEffect = ({
+  evt, inner, outer, quick, brake, pa, pr, enemies,
+}) => (evt.waitTime >= evt.wait
+  ? {
+    state: evt.inputEffect({
+      inner, outer, quick, brake, pa, pr, enemies,
+    }, evt),
+  }
+  : {}
+);
+
+const movePlayer = ({
+  inner, outer, quick, brake, pa, pr,
+}) => ({
+  state: {
+    pa: pa + 0.007 + (quick - brake) * 0.005,
+    pr: min(
+      max(pr + (outer - inner) * 2, 10),
+      boardRadius - 10,
+    ),
+  },
+});
+
+const handleLevelUp = ({ practice, pa, level }) => {
+  const levelUp = pa >= pi2;
+  const nextLevel = level + (levelUp ? 1 : 0);
+  return practice && levelUp && nextLevel % 10 === 1 ? {
+    returns: {
+      nextId: ids.title,
+      nextArgs: [],
+      stateUpdate: {},
+    },
+  } : {
+    state: {
+      pa: pa - (levelUp ? pi2 : 0),
+      level: nextLevel,
+    },
+  };
+};
+
+const drawBoard = ({ pa }) => {
+  clearCanvas();
+  drawBackground();
+  drawTape();
+  drawGuide(pa);
+  drawCenterDot();
+  drawOutline();
+  return {};
+};
+
+const playerPosition = ({ pa, pr }) => ({
+  state: {
+    px: center + pr * cos(-pa),
+    py: center + pr * sin(-pa),
+  },
+});
+
+const drawPlayerIfNeeded = ({ playerInvincible, px, py }) => {
+  if (playerInvincible === 0 || random() < 0.5) {
+    drawPlayer(px, py);
+  }
+  return {};
+};
+
+const runStage = ({
+  mode, level, levelUp, stage, pa, pr, px, py, playerInvincible, enemies, evt,
+}) => {
+  const stageResult = stage(mode, level, levelUp, {
+    px, py, pa, pr, playerInvincible, enemies, evt,
+  });
+  return {
+    state: {
+      stage: stageResult.nextStage,
+      evt: stageResult.evt,
+      enemies: stageResult.enemies,
+    },
+  };
+};
+
+const moveEnemies = ({ enemies, px, py }) => {
+  const moveResult = enemies.reduce((acc, enemy) => {
     const { id } = enemy;
     const { nextEnemies, hit } = enemyIdToMotion.get(id)(enemy, px, py);
     enemyIdToRenderer.get(id)(canvasContext, enemy);
@@ -26,118 +126,102 @@ const moveEnemies = (enemies, px, py) => (
       nextEnemies: [...acc.nextEnemies, ...nextEnemies],
       hit: acc.hit || hit,
     };
-  }, { nextEnemies: [], hit: false })
-);
-
-export default (pauseTime = 0) => ({
-  mode,
-  level: previousLevel,
-  practice,
-  stage,
-  evt: previousEvt,
-  playerAngle,
-  playerRadius,
-  playerInvincible,
-  enemies,
-  deaths,
-}) => {
-  const {
-    inner, outer, quick, brake, pause, escape,
-  } = getInputs();
-  if (escape) {
-    clearCanvas();
-    return {
-      nextId: ids.title,
-      nextArgs: [],
-      stateUpdate: {
-        ...initialState(),
-        mode,
-      },
-    };
-  }
-  if (pause && pauseTime > 10) {
-    return {
-      nextId: ids.pause,
-      nextArgs: [0],
-      stateUpdate: {},
-    };
-  }
-  const stateBeforeEffect = {
-    inner, outer, quick, brake, pa: playerAngle, pr: playerRadius, enemies,
+  }, { nextEnemies: [], hit: false });
+  return {
+    state: {
+      enemies: moveResult.nextEnemies,
+      hit: moveResult.hit,
+    },
   };
-  const effectResult = previousEvt.waitTime >= previousEvt.wait
-    ? previousEvt.inputEffect(stateBeforeEffect, previousEvt)
-    : stateBeforeEffect;
-  let pa = effectResult.pa + 0.007 + (effectResult.quick - effectResult.brake) * 0.005;
-  const pr = min(
-    max(effectResult.pr + (effectResult.outer - effectResult.inner) * 2, 10),
-    boardRadius - 10,
-  );
-  const levelUp = pa >= pi2;
-  const level = previousLevel + (levelUp ? 1 : 0);
-  if (practice && levelUp && level % 10 === 1) {
-    return {
-      nextId: ids.title,
-      nextArgs: [],
-      stateUpdate: {},
-    };
-  }
-  pa -= levelUp ? pi2 : 0;
-  levelView.update(() => ({ level, playerAngle: pa }));
-  modeView.update(() => ({ mode }));
-  clearCanvas();
-  drawBackground();
-  drawTape();
-  drawGuide(pa);
-  const px = center + pr * cos(-pa);
-  const py = center + pr * sin(-pa);
-  if (playerInvincible === 0 || random() < 0.5) {
-    drawPlayer(px, py);
-  }
-  const stageResult = stage(mode, level, levelUp, {
-    px, py, pa, pr, playerInvincible, enemies: effectResult.enemies, evt: previousEvt,
-  });
-  const { evt } = stageResult;
-  const { nextEnemies, hit } = moveEnemies(stageResult.enemies, px, py);
-  drawCenterDot();
-  drawOutline();
+};
+
+const runAfterEffect = ({ evt }) => {
   const {
     eventTime, duration, waitTime, wait, afterEffect, props,
   } = evt;
   const eventActive = waitTime >= wait;
   drawEventGauge(eventActive ? (1 - eventTime / duration) : waitTime / wait);
-  const nextEvt = {
-    ...evt,
-    waitTime: waitTime + 1,
-    eventTime: eventActive ? eventTime + 1 : 0,
-    props: eventActive ? afterEffect(props, eventTime, canvasContext) : props,
-  };
-  eventView.update(() => ({ name: nextEvt.name }));
-  if (hit && playerInvincible === 0) {
-    deathsView.update(() => ({ deaths: deaths + 1 }));
-    return {
-      nextId: ids.death,
-      nextArgs: [],
-      stateUpdate: {
-        level,
-        stage: stageResult.nextStage,
-        deaths: deaths + 1,
-        enemies: nextEnemies,
-        evt: nextEvt,
-      },
-    };
-  }
   return {
-    nextId: ids.main,
-    nextArgs: [pauseTime + 1],
-    stateUpdate: {
-      level,
-      stage: stageResult.nextStage,
-      playerAngle: pa,
-      playerRadius: pr,
-      playerInvincible: max(playerInvincible - 1, 0),
-      enemies: nextEnemies,
-      evt: nextEvt,
+    state: {
+      evt: {
+        ...evt,
+        waitTime: waitTime + 1,
+        eventTime: eventActive ? eventTime + 1 : 0,
+        props: eventActive ? afterEffect(props, eventTime, canvasContext) : props,
+      },
     },
   };
 };
+
+const updateViews = ({
+  mode, level, pa, evt,
+}) => {
+  levelView.update(() => ({ level, playerAngle: pa }));
+  modeView.update(() => ({ mode }));
+  eventView.update(() => ({ name: evt.name }));
+  return {};
+};
+
+const renameShortVariables = ({ pa, pr }) => ({
+  state: {
+    playerAngle: pa,
+    playerRadius: pr,
+  },
+});
+
+const handleDeath = (state) => {
+  const { hit, playerInvincible, deaths } = state;
+  if (hit && playerInvincible === 0) {
+    deathsView.update(() => ({ deaths: deaths + 1 }));
+    return {
+      returns: {
+        nextId: ids.death,
+        nextArgs: [],
+        stateUpdate: {
+          ...state,
+          death: deaths + 1,
+        },
+      },
+    };
+  }
+  return {};
+};
+
+const toNextFrame = (state) => ({
+  returns: {
+    nextId: ids.main,
+    nextArgs: [state.pauseTime + 1],
+    stateUpdate: {
+      ...state,
+      playerInvincible: max(state.playerInvincible - 1, 0),
+    },
+  },
+});
+
+export default (pauseTime = 0) => (previousState) => [
+  getInputsIntoState,
+  handleEscape,
+  handlePause,
+  runInputEffect,
+  movePlayer,
+  handleLevelUp,
+  drawBoard,
+  playerPosition,
+  drawPlayerIfNeeded,
+  runStage,
+  moveEnemies,
+  runAfterEffect,
+  updateViews,
+  renameShortVariables,
+  handleDeath,
+  toNextFrame,
+].reduce((acc, fun, index, arr) => {
+  const { state, returns } = fun(acc);
+  if (returns) arr.splice(index);
+  return { ...acc, ...state, returns };
+}, {
+  ...previousState,
+  pauseTime,
+  pa: previousState.playerAngle,
+  pr: previousState.playerRadius,
+}).returns;
