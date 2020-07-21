790d6718e75c2efef03ca48e25f5020a538858d8
SekiT <takaaki.seki@access-company.com>
2020-07-21 21:02:31 +0900

    Main function does resetTransform

--- event/none.js
+++ event/none.js
@@ -4,15 +4,10 @@ import ids from './ids';
 
 const { infinity } = dependencies.globals;
 
-const afterEffect = (_props, _eventTime, context) => {
-  context.resetTransform();
-};
-
 export default () => makeEvent({
   id: ids.none,
   name: '-',
   wait: infinity,
   duration: 0,
   props: {},
-  afterEffect,
 });
